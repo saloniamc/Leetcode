6b7db6db5319d04eb8409ab66d4b0956a6f763ec
SALONI BAILKAR <Salonisb18.2@gmail.com>
2024-07-26 20:10:20 +0530

    342

---
+++ 342. Power of Four.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n < 1) return false;
+
+        while(n % 4 == 0)
+        {
+            n = n / 4;
+        }
+
+        return (n == 1);
+    }
+};
